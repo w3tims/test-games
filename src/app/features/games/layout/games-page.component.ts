27259fdac13f2f31e39d1049da0e23d214ead696
Tymur Svorovskyi <w3tims@gmail.com>
2020-02-10 12:24:34 +0200

    dirty quickfix loadJackpots interval

--- src/app/features/games/layout/games-page.component.ts
+++ src/app/features/games/layout/games-page.component.ts
@@ -1,27 +1,40 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GamesFacade} from "../../../state/games/games.facade";
-import {GameCategory} from "../types/game-category.enum";
-import {createCategoryFilter} from "../games-helpers";
-import {INavSection} from "../types/nav-item.interface";
 import {gamesNavigationSections} from "../games-navigation-sections";
-import {Router, RoutesRecognized} from "@angular/router";
+import {Router} from "@angular/router";
 import {RouterFacade} from "../../../state/router/router.facade";
-import { isEqual } from 'lodash';
+import {interval, Subscription} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-games-page',
   templateUrl: './games-page.component.html',
   styleUrls: ['./games-page.component.scss']
 })
-export class GamesPageComponent implements OnInit {
+export class GamesPageComponent implements OnInit, OnDestroy {
   activeGamesWithJackpots$ = this.gamesFacade.activeGamesWithJackpots$;
   navSections = gamesNavigationSections;
+
+  subs: Subscription[] = [];
 
   ngOnInit(): void {
     // todo move to effects
     this.routerFacade.navSection$.subscribe(navSection => {
       this.gamesFacade.setFilters(navSection.filters);
     });
+
+    // TODO re-loadJackpots in resolvers ?
+    // temporary dirty fix:
+    this.subs.push(
+      interval(5000)
+        .pipe(
+          tap(() => this.gamesFacade.loadJackpots())
+        ).subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach(sub => sub.unsubscribe());
   }
 
   constructor(
